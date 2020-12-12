1bc39c5c1bc45be5b775b8479f8a8965748db674
sanjay <>
2020-12-12 20:54:48 +0530

    fix location issue

--- Fynoo Services/AppDelegate.swift
+++ Fynoo Services/AppDelegate.swift
@@ -19,8 +19,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
      
-        GMSServices.provideAPIKey("AIzaSyAgZ0AyOGQLcv7loggrrhrPXfPm-jsHguY")
-        GMSPlacesClient.provideAPIKey("AIzaSyAgZ0AyOGQLcv7loggrrhrPXfPm-jsHguY")
+        GMSServices.provideAPIKey("AIzaSyDGzVH50GxKpAAU69gcux1_VMd45G1gJxc")
+        GMSPlacesClient.provideAPIKey("AIzaSyDGzVH50GxKpAAU69gcux1_VMd45G1gJxc")
+        let barColor: UIColor =  _ColorLiteralType(red: 0.9843137255, green: 0.9843137255, blue: 0.9843137255, alpha: 1)
+        let backgroundColor: UIColor =  _ColorLiteralType(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        let textColor: UIColor =  _ColorLiteralType(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        // Navigation bar background.
+        UINavigationBar.appearance().barTintColor = barColor
+        UINavigationBar.appearance().tintColor = .gray
+        // Color and font of typed text in the search bar.
+        let searchBarTextAttributes = [NSAttributedString.Key.foregroundColor: textColor, NSAttributedString.Key.font: UIFont(name: "Helvetica Neue", size: 16)]
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).defaultTextAttributes = searchBarTextAttributes as [NSAttributedString.Key : Any]
+        // Color of the placeholder text in the search bar prior to text entry
+        let placeholderAttributes = [NSAttributedString.Key.foregroundColor: backgroundColor, NSAttributedString.Key.font: UIFont(name: "Helvetica", size: 15)]
+        // Color of the default search text.
+        let attributedPlaceholder = NSAttributedString(string: "Search", attributes: placeholderAttributes as [NSAttributedString.Key : Any])
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).attributedPlaceholder = attributedPlaceholder
           FirebaseApp.configure()
         return true
     }
