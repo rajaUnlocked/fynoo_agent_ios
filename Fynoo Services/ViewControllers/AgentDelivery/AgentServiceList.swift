38bfb586247b4e32dd2624f99092661d4bd65854
Aishwarya <aishwarya2008@gmail.com>
2020-12-14 22:13:16 +0530

    merging

--- Fynoo Services/ViewControllers/AgentDelivery/AgentServiceList.swift
+++ Fynoo Services/ViewControllers/AgentDelivery/AgentServiceList.swift
@@ -19,15 +19,12 @@ class AgentServiceList: UITableViewCell {
     @IBOutlet weak var date: UILabel!
     @IBOutlet weak var orderId: UILabel!
     @IBOutlet weak var name: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-    
 }
