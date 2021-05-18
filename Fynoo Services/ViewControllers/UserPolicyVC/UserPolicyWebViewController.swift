3a5385d57c5c2f779121dce145a7e956423e5a09
neeraj_tiwari <neeraj.tiwari@crebritech.com>
2021-05-18 13:55:00 +0530

    commit

---
+++ Fynoo Services/ViewControllers/UserPolicyVC/UserPolicyWebViewController.swift
@@ -0,0 +1,56 @@
+//
+//  UserPolicyWebViewController.swift
+//  Fynoo
+//
+//  Created by IND-SEN-LP-040 on 18/05/21.
+//  Copyright © 2021 neerajTiwari. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class UserPolicyWebViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
+
+    
+    @IBOutlet weak var headerView: NavigationView!
+    @IBOutlet weak var headerHeightConstant: NSLayoutConstraint!
+    @IBOutlet weak var webView: WKWebView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.navigationDelegate = self
+        self.webView.uiDelegate = self
+        ModalClass.startLoading(self.view
+        )
+        self.setupUI()
+    }
+
+    func setupUI(){
+        self.headerView.menuBtn.isHidden = true
+        self.headerView.titleHeader.text = "User Policy".localized;
+        self.headerView.viewControl = self
+        self.headerHeightConstant.constant = CGFloat(HeaderHeightSingleton.shared.headerHeight)
+        
+        let url = "https://dev.fynoo.com/customer/privacy_policy/"
+        
+        webView.load(URLRequest(url: URL(string: "\(url)")!))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+
+           ModalClass.stopLoading()
+       }
+    
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        
+        if navigationAction.targetFrame == nil {
+            
+            webView.load(navigationAction.request)
+            
+        }
+        
+        return nil
+        
+    }
+}
