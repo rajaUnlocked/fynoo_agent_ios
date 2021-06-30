e4faad92e60404a2d3f0ae02bb124e4bdb07ea73
sanjay <>
2021-06-30 22:02:17 +0530

    client issue

--- Fynoo Services/ViewControllers/Profile/TableViewCell/ProfileDetailTableViewCell.swift
+++ Fynoo Services/ViewControllers/Profile/TableViewCell/ProfileDetailTableViewCell.swift
@@ -16,6 +16,16 @@ protocol ProfileDetailTableViewCellDelegate {
 }
 class ProfileDetailTableViewCell: UITableViewCell {
     var delegate:ProfileDetailTableViewCellDelegate?
+    @IBOutlet weak var followervw: UIView!
+    @IBOutlet weak var branchesvw: UIView!
+    
+    @IBOutlet weak var productsvw: UIView!
+    
+    @IBOutlet weak var likesvw: UIView!
+    
+    @IBOutlet weak var topconstraints: NSLayoutConstraint!
+    
+    
     
     @IBOutlet weak var agentimg: UIImageView!
     @IBOutlet weak var editHeight: NSLayoutConstraint!
