6fba2dc8dda79395c7f4b50574705ddf68eb82ee
neeraj_tiwari <neeraj.tiwari@crebritech.com>
2021-07-14 13:34:29 +0530

    aravind code merge

--- Fynoo Services/ViewControllers/DashboardModule/ResetPassVC/ResetPasswordViewController.swift
+++ Fynoo Services/ViewControllers/DashboardModule/ResetPassVC/ResetPasswordViewController.swift
@@ -119,6 +119,8 @@ class ResetPasswordViewController: UIViewController, UITextFieldDelegate {
         
     }
     
+    
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
